2104d0d7ed06c1e2085ea1109821a807cdd911d9
unknown <jen000@yandex.ru>
2021-11-28 01:51:02 +0300

    Poem homework complete

--- app/src/main/java/example/jf/testapplication/MainActivity.kt
+++ app/src/main/java/example/jf/testapplication/MainActivity.kt
@@ -4,6 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
+/*  О, рассмейтесь, смехачи!
+    О, засмейтесь, смехачи!
+    Что смеются смехами, что смеянствуют смеяльно,
+    О, засмейтесь усмеяльно!
+    О, рассмешищ надсмеяльных — смех усмейных смехачей!
+    О, иссмейся рассмеяльно, смех надсмейных смеячей!
+    Смейево, смейево!
+    Усмей, осмей, смешики, смешики!
+    Смеюнчики, смеюнчики.
+    О, рассмейтесь, смехачи!
+    О, засмейтесь, смехачи! */
+
 const val TAG="MainActivity"
 
 class MainActivity : AppCompatActivity() {
@@ -11,42 +23,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG,"onCreate")
+        Log.d(TAG,"О, рассмейтесь, смехачи!\n" +
+                "О, засмейтесь, смехачи!")
     }
 
     override fun onStart() {
         super.onStart()
 
-        Log.d(TAG,"onStart")
+        Log.d(TAG,"Что смеются смехами, что смеянствуют смеяльно,\n" +
+                "О, засмейтесь усмеяльно!")
     }
 
     override fun onResume() {
         super.onResume()
 
-        Log.d(TAG,"onResume")
+        Log.d(TAG,"О, рассмешищ надсмеяльных — смех усмейных смехачей!")
     }
 
     override fun onPause() {
         super.onPause()
 
-        Log.d(TAG,"onPause")
+        Log.d(TAG,"О, иссмейся рассмеяльно, смех надсмейных смеячей!\n")
     }
 
     override fun onStop() {
         super.onStop()
 
-        Log.d(TAG,"onStop")
+        Log.d(TAG,"Смейево, смейево!\n" +
+                "Усмей, осмей, смешики, смешики!")
     }
 
     override fun onRestart() {
         super.onRestart()
 
-        Log.d(TAG,"onCreate")
+        Log.d(TAG,"Смеюнчики, смеюнчики.")
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Log.d(TAG,"onDestroy")
+        Log.d(TAG,"О, рассмейтесь, смехачи!\n" +
+                "О, засмейтесь, смехачи!")
     }
 }
